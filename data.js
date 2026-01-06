9f55269e655785135ee3f025219eb4415405f8e5
vohobjonovasevinch-coder <vohobjonovasevinch@gmail.com>
2026-01-06 11:24:25 +0500

    data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const dictionary = [
+  { en: "book", uz: "kitob", ru: "книга" },
+  { en: "water", uz: "suv", ru: "вода" },
+  { en: "school", uz: "maktab", ru: "школа" },
+  { en: "computer", uz: "kompyuter", ru: "компьютер" }
